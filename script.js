4974048991ddaf55e1491b43ed843615e3d45afc
Annabelle Perbet <annabelleperbet@yahoo.fr>
2024-12-26 18:46:04 +0100

    Ecoute de l'événement click sur le bouton valider

--- script.js
+++ script.js
@@ -10,18 +10,40 @@ const deliveryDelayExpress = document.querySelector(
   ".delivery__delay__express"
 );
 const deliveryDay = document.querySelector(".description__content");
+const button = document.querySelector(".btn");
 
 // Ecoute de l'événement "click" sur l'input deliveryStandardInput
 deliveryStandardInput.addEventListener("click", () => {
   deliveryDay.textContent = deliveryDelayStandard.textContent;
+  // Ecoute de l'événement "click" sur le bouton valider
+  button.addEventListener("click", () => {
+    alert(
+      `Votre colis numéro 69249164514354856 vous sera livré sous ${deliveryDelayStandard.textContent} ! Un mail vient de vous être envoyé. Merci de votre confiance !`
+    );
+    window.location.reload();
+  });
 });
 
 // Ecoute de l'événement "click" sur l'input deliveryFastInput
 deliveryFastInput.addEventListener("click", () => {
   deliveryDay.textContent = deliveryDelayFast.textContent;
+  // Ecoute de l'événement "click" sur le bouton valider
+  button.addEventListener("click", () => {
+    alert(
+      `Votre colis numéro 69249164514354856 vous sera livré sous ${deliveryDelayFast.textContent} ! Un mail vient de vous être envoyé. Merci de votre confiance !`
+    );
+    window.location.reload();
+  });
 });
 
 // Ecoute de l'événement "click" sur l'input deliveryExpressInput
 deliveryExpressInput.addEventListener("click", () => {
   deliveryDay.textContent = deliveryDelayExpress.textContent;
+  // Ecoute de l'événement "click" sur le bouton valider
+  button.addEventListener("click", () => {
+    alert(
+      `Votre colis numéro 69249164514354856 vous sera livré sous ${deliveryDelayExpress.textContent} ! Un mail vient de vous être envoyé. Merci de votre confiance ! `
+    );
+    window.location.reload();
+  });
 });
